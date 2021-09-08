1fbf4552af21f639fdc60b5d204b33771b93fc72
doitduri <kdle_@naver.com>
2021-09-08 10:26:08 +0900

    화면간 이동 (delegate pattern)
    - 프로토콜 정의
    - 값을 전달 받을 메소드 선언, 값 가져오기

--- ScreenTransactionExample/ScreenTransactionExample/CodePresentViewController.swift
+++ ScreenTransactionExample/ScreenTransactionExample/CodePresentViewController.swift
@@ -7,15 +7,28 @@
 
 import UIKit
 
-class CodePresentViewController: UIViewController {
+protocol SendDataDelegate: AnyObject {
+    func sendData(name: String)
+}
 
+class CodePresentViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var name: String?
+    weak var delegate: SendDataDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
     }
     
     @IBAction func tabBackButton(_ sender: UIButton) {
+        self.delegate?.sendData(name: "duri")
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
-
+    
 }
