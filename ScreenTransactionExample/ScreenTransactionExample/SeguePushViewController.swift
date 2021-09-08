a89121455de03f69d2eaa2bbd0ecada543110320
doitduri <kdle_@naver.com>
2021-09-08 10:29:03 +0900

    화면간 이동 (segue)
    전달 받고 보여줄 nameLabel 선언
    
    // 이전 life-cycle 코드도 추가

--- ScreenTransactionExample/ScreenTransactionExample/SeguePushViewController.swift
+++ ScreenTransactionExample/ScreenTransactionExample/SeguePushViewController.swift
@@ -9,12 +9,44 @@ import UIKit
 
 class SeguePushViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
     @IBAction func tabBackButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
         self.navigationController?.popToRootViewController(animated: true)
     }
     
+    var name: String?
+    
+    
+    // MARK Life-Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Segue viewDidLoad - 뷰가 로드 됐어")
+        
+        if let name = name {
+            self.nameLabel.text = name
+            self.nameLabel.sizeToFit()
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Segue viewWillAppear - 뷰가 나타날꺼야")
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("Segue viewDidAppear - 뷰가 나타났어")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Segue viewWillDisappear - 뷰가 곧 사라질꺼야..")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("Segue viewDidDisappear - 뷰가 사라졌어")
     }
 }
